1f6f465124d517132035e8e9b9620876cb614bbe
jcpanggoy <jcpanggoy@mcm.edu.ph>
2024-07-31 03:11:20 +0800

    Worked on Projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,5 @@
-// features: website (if possible), github, manuscript (if applicable)
-import React from 'react'
-import Title from '../layouts/Title'
+import React from 'react';
+import Title from '../layouts/Title';
 import { projectOne, projectTwo, projectThree } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
@@ -19,55 +18,63 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="WAISMAIS"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="An IoT-Enhanced Corn Disease Diagnosis through a Mobile Application Using Decision Tree Classifier for Soil NPK Analysis and YOLOv8 for Disease Identification."
           src={projectOne}
+          githubLink="https://github.com/miyukiandjia/Thesis-WAISMAIS.git"
+          liveLink="https://drive.google.com/file/d/1r9rUzeNv6GkTiKy6l6iQH5f5mThJrwxB/view?usp=sharing"
         />
         <ProjectsCard
-          title="IFO Booking System"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="MMCM IFO Booking System"
+          des="An intuitive platform that streamlines resource requests and reservations, ensuring efficient management and real-time availability tracking within MMCM."
           src={projectTwo}
+          githubLink="https://github.com/Lolillya/mcm_AppDev-bookingApp.git"
+          liveLink="https://ifo-booking-system-live-link.com"
         />
         <ProjectsCard
           title="LAKBAYAN"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="A Travel-Social Itinerary Mobile Application with Derived Enhanced Simulated Annealing Algorithm."
           src={projectThree}
+          githubLink="https://github.com/miyukiandjia/lakbayan.git"
+          liveLink="https://www.sciencedirect.com/science/article/pii/S1877050924006501?dgcid=rss_sd_all"
         />
         <ProjectsCard
           title="FIELDMATE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectThree}
+          githubLink="https://github.com/jcpanggoy/fieldmate.git"
+          liveLink="https://drive.google.com/file/d/1S5UHVhiMjdi0_qwXPx-BsmxyOikxPddO/view?usp=sharing"
         />
         <ProjectsCard
           title="GWA Calculator"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectOne}
+          githubLink="https://github.com/your-username/gwa-calculator"
+          liveLink="https://gwa-calculator-live-link.com"
         />
         <ProjectsCard
           title="Pizza Inventory System"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          githubLink="https://github.com/your-username/pizza-inventory-system"
+          liveLink="https://pizza-inventory-system-live-link.com"
         />
         <ProjectsCard
           title="KISLAP: Tagalog Programming Language"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          githubLink="https://github.com/your-username/kislap-tagalog-programming-language"
+          liveLink="https://kislap-tagalog-programming-language-live-link.com"
         />
         <ProjectsCard
           title="Expenses Calculator"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          githubLink="https://github.com/your-username/expenses-calculator"
+          liveLink="https://expenses-calculator-live-link.com"
         />
       </div>
     </section>
   );
 }
 
-export default Projects
+export default Projects;
