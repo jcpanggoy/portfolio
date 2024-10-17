7caa128f45ef207d83e893f5c5e6ee21da81ebe3
jcpanggoy <jcpanggoy@mcm.edu.ph>
2024-10-18 03:07:50 +0800

    Certificates Page Done

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe } from "react-icons/fa";
-import { SiProgress, SiAntdesign } from "react-icons/si";
+import { AiOutlineCode, AiFillMobile, AiOutlineGlobal } from "react-icons/ai";
+import { FaPython, FaJava, FaLaptopCode } from "react-icons/fa";
 import Title from "../layouts/Title";
 import Card from "./Card";
 
@@ -14,39 +13,40 @@ const Features = () => {
       <Title title="Proof of Excellence" des="Certificates" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <Card
-          title="Business Stratagy"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          title="Software Development Process and Methodologies"
+          des="Learned software development life cycles: Agile, Scrum, and Waterfall."
+          icon={<AiOutlineCode />}
+          link="https://drive.google.com/file/d/18Hp4GRPkHYexolM1mNyxqkcTWn14enfo/view?usp=sharing"
         />
         <Card
-          title="App Development"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<AiFillAppstore />}
+          title="Career Skills in Software Development"
+          des="Learned key concepts of developing software applications."
+          icon={<FaLaptopCode />}
+          link="https://drive.google.com/file/d/1eYNL7BKSaMNW-xBE-PxP7zv1tYt4RbAQ/view?usp=sharing"
         />
         <Card
-          title="SEO Optimisation"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<SiProgress />}
+          title="Career Essentials in Software Development"
+          des="Key skills acquired through training provided by Microsoft and LinkedIn."
+          icon={<AiFillMobile />}
+          link="https://drive.google.com/file/d/16hS4Ui4Y1wpbIEJJMY7_y5wCJYVFMOKZ/view?usp=sharing"
         />
         <Card
-          title="Mobile Development"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<FaMobile />}
+          title="Introduction to Data Science in Python"
+          des="Mastered the basics of data science programming with Python."
+          icon={<FaPython />}
+          link="https://drive.google.com/file/d/1vuViFvEi-Mw2X55mPcaMRxhLEceUxUts/view?usp=sharing"
         />
         <Card
-          title="UX Design"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<SiAntdesign />}
+          title="Java Object-Oriented Programming"
+          des="Acquired skills in Java programming and object-oriented concepts."
+          icon={<FaJava />}
+          link="https://drive.google.com/file/d/1wnMVRR3IQecl9N91dDdfm31ddglfAW-T/view?usp=sharing"
         />
         <Card
-          title="Hosting Websites"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<FaGlobe />}
+          title="Programming Foundations Beyond the Fundamentals"
+          des="Developed advanced programming skills beyond basic concepts."
+          icon={<AiOutlineGlobal />}
+          link="https://drive.google.com/file/d/1GOuSzyFH26wXv4H18cL8nHjbZ_BtHnAA/view?usp=sharing"
         />
       </div>
     </section>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -11,31 +11,24 @@ const Projects = () => {
     >
       <div className="flex justify-center items-center text-center">
         <Title
-          title="Check Out My Works – I'd Love to Hear Your Feedback!"
-          des="My Projects"
+          title="Check Out My Works! I'd Love to Hear Your Feedback!"
+          des="Projects"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        <ProjectsCard
-          title="WAISMAIS"
-          des="An IoT-Enhanced Corn Disease Diagnosis through a Mobile Application Using Decision Tree Classifier for Soil NPK Analysis and YOLOv8 for Disease Identification."
-          src={projectOne}
-          githubLink="https://github.com/miyukiandjia/Thesis-WAISMAIS.git"
-          liveLink="https://drive.google.com/file/d/1r9rUzeNv6GkTiKy6l6iQH5f5mThJrwxB/view?usp=sharing"
-        />
-        <ProjectsCard
-          title="MMCM IFO Booking System"
-          des="An intuitive platform that streamlines resource requests and reservations, ensuring efficient management and real-time availability tracking within MMCM."
-          src={projectTwo}
-          githubLink="https://github.com/Lolillya/mcm_AppDev-bookingApp.git"
-          liveLink="https://ifo-booking-system-live-link.com"
-        />
         <ProjectsCard
           title="LAKBAYAN"
           des="A Travel-Social Itinerary Mobile Application with Derived Enhanced Simulated Annealing Algorithm."
           src={projectThree}
           githubLink="https://github.com/miyukiandjia/lakbayan.git"
           liveLink="https://www.sciencedirect.com/science/article/pii/S1877050924006501?dgcid=rss_sd_all"
+        />
+        <ProjectsCard
+          title="WAISMAIS"
+          des="An IoT-Enhanced Corn Disease Diagnosis through a Mobile Application Using Decision Tree Classifier for Soil NPK Analysis and YOLOv8 for Disease Identification."
+          src={projectOne}
+          githubLink="https://github.com/miyukiandjia/Thesis-WAISMAIS.git"
+          liveLink="https://drive.google.com/file/d/1r9rUzeNv6GkTiKy6l6iQH5f5mThJrwxB/view?usp=sharing"
         />
         <ProjectsCard
           title="FIELDMATE"
@@ -45,20 +38,6 @@ const Projects = () => {
           liveLink="https://drive.google.com/file/d/1S5UHVhiMjdi0_qwXPx-BsmxyOikxPddO/view?usp=sharing"
         />
         <ProjectsCard
-          title="GWA Calculator"
-          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
-          githubLink="https://github.com/your-username/gwa-calculator"
-          liveLink="https://gwa-calculator-live-link.com"
-        />
-        <ProjectsCard
-          title="Pizza Inventory System"
-          des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
-          githubLink="https://github.com/your-username/pizza-inventory-system"
-          liveLink="https://pizza-inventory-system-live-link.com"
-        />
-        <ProjectsCard
           title="KISLAP: Tagalog Programming Language"
           des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
@@ -66,11 +45,18 @@ const Projects = () => {
           liveLink="https://kislap-tagalog-programming-language-live-link.com"
         />
         <ProjectsCard
-          title="Expenses Calculator"
+          title="MMCM IFO Booking System"
+          des="An intuitive platform that streamlines resource requests and reservations, ensuring efficient management and real-time availability tracking within MMCM."
+          src={projectTwo}
+          githubLink="https://github.com/Lolillya/mcm_AppDev-bookingApp.git"
+          liveLink="https://ifo-booking-system-live-link.com"
+        />
+        <ProjectsCard
+          title="Pizza Inventory System"
           des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
-          githubLink="https://github.com/your-username/expenses-calculator"
-          liveLink="https://expenses-calculator-live-link.com"
+          githubLink="https://github.com/your-username/pizza-inventory-system"
+          liveLink="https://pizza-inventory-system-live-link.com"
         />
       </div>
     </section>
